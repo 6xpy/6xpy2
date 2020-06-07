9d88cd272f54682ec7c77653338bb33b400f1e83
6xpy <66048447+6xpy@users.noreply.github.com>
2020-06-07 18:16:51 -0400

    Update bot.js

--- mikesbot-repo-master/bot.js
+++ mikesbot-repo-master/bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login("NzE4OTk5Mjg5MzY3NDk0NzI2.Xt0seA.LWiuZd00rCrYOvz_HvH9Y7xwNtk")
+client.login("NzE4OTk5Mjg5MzY3NDk0NzI2.Xt1nMw.OYIPB6RqYKDzEf6JPzr5Dg7Jlsw")
 
 client.on('ready', () => (
  console.log("readyl")
